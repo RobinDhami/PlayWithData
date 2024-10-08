ff67fcd9fa849f4c61251c0bc0e55f95425ca3ec
RobinDhami <rozendhami69@gmail.com>
2024-10-08 23:02:32 +0545

    django forms

--- Data/website/views.py
+++ Data/website/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Member
+from .forms import MemberForm
+
 # Create your views here.
 def home(request):
     all_members = Member.objects.all()
-    return render(request,'home.html',{"mem":all_members})
+    return render(request, 'home.html', {"mem": all_members})
 
-def log(request):
-    return render(request,'login.html',{})
+def join(request):
+    if request.method == "POST":
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('join')  # Change this to the correct URL pattern name
+    else:
+        form = MemberForm()  # Initialize an empty form
+
+    return render(request, 'login.html', {'form': form})
